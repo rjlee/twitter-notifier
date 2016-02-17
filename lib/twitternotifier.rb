106c608a2cdcf266c1e2db1ce367c740c310148c
Robert Lee <robl@rjlee.net>
2016-02-17 19:47:57 +0000

    Beautify

--- lib/twitternotifier.rb
+++ lib/twitternotifier.rb
@@ -28,38 +28,38 @@ class TwitterNotifier
     client = connect
     recent_tweet_id = nil
     while loop? do
-      soptions = recent_tweet_id.nil? ? {} : {:since_id => recent_tweet_id}
-      puts "Searching with options:\n#{soptions}" if @options.verbose
+        soptions = recent_tweet_id.nil? ? {} : {:since_id => recent_tweet_id}
+        puts "Searching with options:\n#{soptions}" if @options.verbose
 
-      tweets = client.search(@options.search, soptions).take(100).collect
-      tweets.each do |tweet|
-        # Match either tweets that are new if we haven't seen any tweets before or tweets since the last most
-        # recent tweet if we have seen tweets before.  This means it will send notifications for tweets
-        # sent during being laptop being suspended and woken up
-        if (recent_tweet_id.nil? && tweet.created_at >= Time.now-@options.delay) or (!recent_tweet_id.nil?)
-          puts "#{tweet.user.screen_name}: #{tweet.text}" if @options.verbose
-          TerminalNotifier.notify("#{tweet.user.screen_name}: #{tweet.text}", :title => 'Twitter Search', :appIcon => './assets/twitter.png', :sender => 'com.twitter.twitter-mac', :open => tweet.uri)
+        tweets = client.search(@options.search, soptions).take(100).collect
+        tweets.each do |tweet|
+          # Match either tweets that are new if we haven't seen any tweets before or tweets since the last most
+          # recent tweet if we have seen tweets before.  This means it will send notifications for tweets
+          # sent during being laptop being suspended and woken up
+          if (recent_tweet_id.nil? && tweet.created_at >= Time.now-@options.delay) or (!recent_tweet_id.nil?)
+            puts "#{tweet.user.screen_name}: #{tweet.text}" if @options.verbose
+            TerminalNotifier.notify("#{tweet.user.screen_name}: #{tweet.text}", :title => 'Twitter Search', :appIcon => './assets/twitter.png', :sender => 'com.twitter.twitter-mac', :open => tweet.uri)
+          end
         end
+        recent_tweet_id = tweets.first.id if tweets.count > 0
+
+        puts "Sleeping for #{@options.delay} secs" if @options.verbose
+        sleep(@options.delay)
       end
-      recent_tweet_id = tweets.first.id if tweets.count > 0
+    end
 
-      puts "Sleeping for #{@options.delay} secs" if @options.verbose
-      sleep(@options.delay)
+
+    private
+
+    def loop?()
+      true
+    end
+
+    def connect
+      Twitter::REST::Client.new do |config|
+        config.consumer_key        = @options.consumer_key
+        config.consumer_secret     = @options.consumer_secret
+        config.proxy = @options.proxy
+      end
     end
   end
-
-
-  private
-
-  def loop?()
-  	true
-  end
-
-  def connect
-    Twitter::REST::Client.new do |config|
-      config.consumer_key        = @options.consumer_key
-      config.consumer_secret     = @options.consumer_secret
-      config.proxy = @options.proxy
-    end
-  end
-end
