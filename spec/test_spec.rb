01d2aa36d4a10444b8d25c8fed2e614542a569d5
Robert Lee <robl@rjlee.net>
2016-02-17 00:55:23 +0000

    Removing details

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe TwitterNotifier do
     expect(tn.options.verbose).to eq(TwitterNotifier::VERBOSE)
     expect(tn.options.quiet).to eq(TwitterNotifier::QUIET)
   end
-  
+
   # http://stackoverflow.com/questions/13365366/rspec-unit-testing-a-method-which-has-a-infinite-loop
   # http://stackoverflow.com/questions/5717813/what-is-the-best-practice-when-it-comes-to-testing-infinite-loops/33348498#33348498
   #it "runs" do
   #	TwitterNotifier.expects(:loop).yields().then().returns()
-  #	TwitterNotifier.new(delay: 1, search: "securitay", consumer_key: "dPbjTJeOc6sBgU7ZclQmT5yU4", consumer_secret: "6QtYXq4vhpgfeEiG19V8Zff7xWDeEagR0uTWPTg5UPews1lIta", verbose: true).run
+  #	TwitterNotifier.new(delay: 1, search: "securitay", consumer_key: "", consumer_secret: "", verbose: true).run
   #end
 
 end
